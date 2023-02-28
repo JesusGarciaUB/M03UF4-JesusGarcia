274af754445a5ec58e6e2fc821d54b34ccad02f8
JesusGarciaUB <jesus.garcia@enti.cat>
2023-02-28 14:21:29 +0100

    a

--- LastStruct/Main.cpp
+++ LastStruct/Main.cpp
@@ -9,18 +9,16 @@ int main() {
 	srand(time(NULL));
 	Enemy enemies[MAX_ENEMIES];
 	bool rep;
-	int currentEnemies = 0;
 
 	for (int x = 0; x < MAX_ENEMIES; x++) {
 		rep = true;
 		while (rep) {
 			enemies[x] = createRandomEnemy();
-			if (currentEnemies != 0) {
-				for (int i = 0; i < currentEnemies; i++) rep = equalEnemies(enemies[x], enemies[i]);
+			if (x != 0) {
+				for (int i = 0; i < x; i++) rep = equalEnemies(enemies[x], enemies[i]);
 			}
 			else rep = false;
 		}
-		currentEnemies++;
 	}
 
 	for (Enemy& e : enemies) {
